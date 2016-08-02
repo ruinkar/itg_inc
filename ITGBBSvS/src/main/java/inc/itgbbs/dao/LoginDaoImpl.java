dfef911f8994101886cdd30d8b30cec2932f636d
INC-B-14 <INC-B-14@INC>
2016-08-02 18:23:53 +0900

    working validate

--- ITGBBSvS/src/main/java/inc/itgbbs/dao/LoginDaoImpl.java
+++ ITGBBSvS/src/main/java/inc/itgbbs/dao/LoginDaoImpl.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.stereotype.Component;
 
 import inc.itgbbs.domain.LoginInfoDTO;
+import inc.itgbbs.domain.MemberCommand;
 
 @Component
 public class LoginDaoImpl extends SqlSessionDaoSupport 
@@ -13,6 +14,17 @@ public class LoginDaoImpl extends SqlSessionDaoSupport
 	public Integer findUser(LoginInfoDTO login) {
 		// TODO Auto-generated method stub
 		return getSqlSession().selectOne("Login.findUser",login);
+	}
+
+	@Override
+	public void signUpMember(MemberCommand memberCommand) {
+		getSqlSession().insert("Login.signUpMember", memberCommand);
+		getSqlSession().insert("Login.signUpLoginInfo", memberCommand);
+	}
+
+	@Override
+	public void deleteLoginInfo(LoginInfoDTO loginInfoDTO) {
+		getSqlSession().delete("Login.deleteLoginInfo", loginInfoDTO);
 	}
 
 	
