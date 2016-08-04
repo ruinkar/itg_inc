671786de081da73e6e09fe2f5c420f18702c147e
INC-B-14 <INC-B-14@INC>
2016-08-04 16:42:48 +0900

    rebase

--- ITGBBSvS/src/main/java/inc/itgbbs/validator/UniqueValidator.java
+++ ITGBBSvS/src/main/java/inc/itgbbs/validator/UniqueValidator.java
@@ -8,28 +8,34 @@ import org.springframework.stereotype.Component;
 
 import inc.itgbbs.dao.LoginDao;
 
+
 @Component
-public class UniqueValidator implements ConstraintValidator<Unique, Object> {
+public class UniqueValidator implements ConstraintValidator<Unique, /*Object*/String> {
 
 	@Autowired
 	LoginDao loginDao;
-	
-	String col;
+	/*
+	String colName;
+	String tabName;
+	String fldName;*/
 	
 	@Override
 	public void initialize(Unique constraintAnnotation) {
-		this.col = constraintAnnotation.col();
+		/*
+		this.tabName = constraintAnnotation.tabName();
+		this.colName = constraintAnnotation.colName();
+		this.fldName = constraintAnnotation.fldName();*/
 	}
 
 	@Override
-	public boolean isValid(Object value, ConstraintValidatorContext context) {
+	public boolean isValid(String value, ConstraintValidatorContext context) {
+		System.out.println("validating unique");
 		
-		if (loginDao.getMember(value.toString()) == 0) {
+		if (loginDao.getMember(value) > 0) {
 			return false;
 		} else {
 			return true;
 		}
-		
 	}
 	
 }
